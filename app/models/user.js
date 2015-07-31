f4bd8a695f59699b364dbba7f6264fd36197cb71
tranbenny <tran.benny123@gmail.com>
2015-07-31 15:22:14 -0700

    edited user model

--- app/models/user.js
+++ app/models/user.js
@@ -3,7 +3,11 @@ var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
 	name : String,
-	email : String
+	email : String, 
+	toDoItems : {
+		text : String,
+		done : Boolean
+	}
 });
 
 module.exports = mongoose.model('User', UserSchema);
